6a4e0af2b2517d3bb00f1fcff62c96a7b06070cd
Michael Ngai <michael.ngai@tufts.edu>
2022-03-24 21:53:24 -0400

    improved button aesthetics

--- src/demo.jsx
+++ src/demo.jsx
@@ -9,10 +9,24 @@ const ReactBarGraph = React.lazy(() => import('./lib'))
 function Component({ getDemo }) {
 
   const demo = getDemo()
+  const palette = 
+    ["#d89735",
+    "#7c60cd",
+    "#59b74b",
+    "#c063b9",
+    "#a7b541",
+    "#6d83c9",
+    "#d0453d",
+    "#48b6d2",
+    "#c5704d",
+    "#5bb78b",
+    "#c75980",
+    "#53803b",
+    "#9b8542"]
 
   return (
     <div style={{ display: 'flex', flex: '1 1 auto', overflow: 'hidden', margin: '5%' }}>
-      <ReactBarGraph style={ReactBarGraphStyle} title={"Demo React Bar Graph"} orientation={"horizontal"} data={demo} />
+      <ReactBarGraph style={ReactBarGraphStyle} title={"Demo React Bar Graph"} orientation={"horizontal"} palette={palette} data={demo} />
     </div>
   )
 }
