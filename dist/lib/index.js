fe649ba8050dd6a8f2c31203f342d348f9b91aff
Daniel Clarke <u8sand@gmail.com>
2022-03-28 12:52:10 -0400

    Rebuild JS

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,10 +1,20 @@
+import _asyncToGenerator from "@babel/runtime/helpers/asyncToGenerator";
 import _slicedToArray from "@babel/runtime/helpers/slicedToArray";
 import _typeof from "@babel/runtime/helpers/typeof";
+import _regeneratorRuntime from "@babel/runtime/regenerator";
 import React, { useState } from 'react';
 import 'chart.js/auto';
 import { Bar } from 'react-chartjs-2';
 import RotateLeftIcon from '@mui/icons-material/RotateLeft';
 import RotateRightIcon from '@mui/icons-material/RotateRight';
+import DownloadIcon from '@mui/icons-material/Download';
+import { IconButton } from '@mui/material';
+import Tooltip from '@mui/material/Tooltip';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
+import { saveAs } from 'file-saver';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 function classes() {
   for (var _len = arguments.length, C = new Array(_len), _key = 0; _key < _len; _key++) {
@@ -26,16 +36,31 @@ function classes() {
   }
 }
 
-var palette = ["#ce9197", "#8ee400", "#c600ff", "#e7ff32", "#5e44ff", "#e8cc2a", "#005cff", "#ffb800", "#3e41cc", "#27d370", "#ff3bff", "#006f00", "#ff75ff", "#006c00", "#000078", "#68de84", "#002aa6", "#ff8700", "#0059cc", "#ff3f00", "#00ffff", "#ba0000", "#00ffc9", "#ff338c", "#00c97c", "#3d2ca1", "#ff6002", "#0085f1", "#b14400", "#00ffff", "#ff355b", "#00d8f1", "#ff386c", "#7ef2d1", "#000348", "#df9040", "#004bae", "#ff995d", "#0049a1", "#876500", "#bf84ff", "#00651a", "#f582ff", "#006820", "#ff9cff", "#004600", "#ff9bed", "#003000", "#ffabfe", "#002c00", "#ffa7ef", "#002800", "#5697ff", "#873600", "#00a9ff", "#ff795b", "#00a9ff", "#363900", "#4888ef", "#0d2a00", "#9092f3", "#002600", "#95389a", "#00542f", "#f26f98", "#4fe7ff", "#370000", "#bbffff", "#420014", "#32dcff", "#350500", "#00c6fd", "#351000", "#00b7ff", "#ec856c", "#00b7ff", "#783424", "#00aff1", "#ff919a", "#002061", "#e0beae", "#0057ab", "#bc7663", "#75affc", "#003e33", "#ff8dbb", "#002931", "#ffd3ff", "#007470", "#ff99b5", "#00aec2", "#e8a8d5", "#798275", "#e0bdff", "#6e5c7c", "#88d6ff", "#b599d1", "#cee1f5", "#9e9c99", "#9bc3e3"];
 export default function ReactBarGraph(props) {
   var _useState = useState(props.orientation),
       _useState2 = _slicedToArray(_useState, 2),
       orientation = _useState2[0],
       setOrientation = _useState2[1];
 
+  var _React$useState = React.useState(null),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      anchorEl = _React$useState2[0],
+      setAnchorEl = _React$useState2[1];
+
   React.useEffect(function () {
     if (props.orientation !== undefined) setOrientation(props.orientation);
   }, [props.orientation]);
+  var open = Boolean(anchorEl);
+
+  var handleClick = function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  };
+
+  var handleClose = function handleClose() {
+    setAnchorEl(null);
+  };
+
+  var printRef = React.useRef();
 
   function swapOrientation() {
     if (orientation === "horizontal") {
@@ -45,44 +70,113 @@ export default function ReactBarGraph(props) {
     }
   }
 
+  var handleDownloadToPNG = /*#__PURE__*/function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee() {
+      var canvasSave;
+      return _regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              canvasSave = document.getElementById('graph');
+              canvasSave.toBlob(function (blob) {
+                saveAs(blob, "graph.png");
+              });
+              handleClose();
+
+            case 3:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee);
+    }));
+
+    return function handleDownloadToPNG() {
+      return _ref.apply(this, arguments);
+    };
+  }();
+
+  var handleDownloadToSVG = /*#__PURE__*/function () {
+    var _ref2 = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee2() {
+      return _regeneratorRuntime.wrap(function _callee2$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+            case "end":
+              return _context2.stop();
+          }
+        }
+      }, _callee2);
+    }));
+
+    return function handleDownloadToSVG() {
+      return _ref2.apply(this, arguments);
+    };
+  }();
+
+  var handleDownloadToEPS = /*#__PURE__*/function () {
+    var _ref3 = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee3() {
+      return _regeneratorRuntime.wrap(function _callee3$(_context3) {
+        while (1) {
+          switch (_context3.prev = _context3.next) {
+            case 0:
+            case "end":
+              return _context3.stop();
+          }
+        }
+      }, _callee3);
+    }));
+
+    return function handleDownloadToEPS() {
+      return _ref3.apply(this, arguments);
+    };
+  }();
+
+  var handleDownloadToPDF = /*#__PURE__*/function () {
+    var _ref4 = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee4() {
+      var element, canvas, data, pdf, imgProperties, pdfWidth, pdfHeight;
+      return _regeneratorRuntime.wrap(function _callee4$(_context4) {
+        while (1) {
+          switch (_context4.prev = _context4.next) {
+            case 0:
+              element = printRef.current;
+              _context4.next = 3;
+              return html2canvas(element);
+
+            case 3:
+              canvas = _context4.sent;
+              data = canvas.toDataURL('image/png');
+              pdf = new jsPDF();
+              imgProperties = pdf.getImageProperties(data);
+              pdfWidth = pdf.internal.pageSize.getWidth();
+              pdfHeight = imgProperties.height * pdfWidth / imgProperties.width;
+              pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
+              pdf.save('graph.pdf');
+              handleClose();
+
+            case 12:
+            case "end":
+              return _context4.stop();
+          }
+        }
+      }, _callee4);
+    }));
+
+    return function handleDownloadToPDF() {
+      return _ref4.apply(this, arguments);
+    };
+  }();
+
   var data = props.data;
   var ids = new Array(data.length);
-  var scores = new Array(data.length);
-  var colors = new Array(data.length); // deconstructing the JSON object into ids and scores
+  var scores = new Array(data.length); // deconstructing the JSON object into ids and scores
 
   for (var i = 0; i < data.length; i++) {
     ids[i] = data[i].id;
     scores[i] = data[i].score;
   }
 
-  var paletteIndex = 0;
-
-  for (var _i = 0; _i < data.length; _i++) {
-    if (colors[_i] == undefined) {
-      colors[_i] = palette[paletteIndex];
-      var firstWord = ids[_i];
-
-      if (ids[_i].indexOf('_') != -1) {
-        firstWord = ids[_i].substring(0, ids[_i].indexOf('_'));
-      }
-
-      for (var j = _i + 1; j < data.length; j++) {
-        var firstWord2 = ids[j];
-
-        if (ids[j].indexOf('_') != -1) {
-          firstWord2 = ids[j].substring(0, ids[j].indexOf('_'));
-        }
-
-        if (firstWord === firstWord2) {
-          colors[j] = palette[paletteIndex];
-        }
-      }
-
-      paletteIndex++;
-      if (paletteIndex >= paletteIndex.size) paletteIndex = 0;
-    }
-  }
-
+  var colors = props.palette !== undefined ? props.palette : ["blue"];
   var graphData = {
     labels: ids,
     datasets: [{
@@ -134,18 +228,56 @@ export default function ReactBarGraph(props) {
 
   return /*#__PURE__*/React.createElement("div", {
     className: props.style.layout
-  }, orientation == "vertical" ? /*#__PURE__*/React.createElement(RotateRightIcon, {
-    className: props.style.button,
+  }, /*#__PURE__*/React.createElement("div", {
+    className: props.style.toolBar
+  }, orientation == "vertical" ? /*#__PURE__*/React.createElement(Tooltip, {
+    title: "Horizontal View",
+    placement: "top"
+  }, /*#__PURE__*/React.createElement(IconButton, {
     onClick: function onClick() {
       return swapOrientation();
-    }
-  }) : /*#__PURE__*/React.createElement(RotateLeftIcon, {
-    className: props.style.button,
+    },
+    className: props.style.button
+  }, /*#__PURE__*/React.createElement(RotateRightIcon, null))) : /*#__PURE__*/React.createElement(Tooltip, {
+    title: "Vertical View",
+    placement: "top"
+  }, /*#__PURE__*/React.createElement(IconButton, {
     onClick: function onClick() {
       return swapOrientation();
+    },
+    className: props.style.button
+  }, /*#__PURE__*/React.createElement(RotateLeftIcon, null))), /*#__PURE__*/React.createElement(Tooltip, {
+    title: "Download Graph",
+    placement: "top"
+  }, /*#__PURE__*/React.createElement(IconButton, {
+    id: "basic-button",
+    "aria-controls": open ? 'basic-menu' : undefined,
+    "aria-haspopup": "true",
+    "aria-expanded": open ? 'true' : undefined,
+    onClick: handleClick,
+    className: props.style.button
+  }, /*#__PURE__*/React.createElement(DownloadIcon, null))), /*#__PURE__*/React.createElement(Menu, {
+    id: "basic-menu",
+    anchorEl: anchorEl,
+    open: open,
+    onClose: handleClose,
+    MenuListProps: {
+      'aria-labelledby': 'basic-button'
     }
-  }), /*#__PURE__*/React.createElement(Bar, {
+  }, /*#__PURE__*/React.createElement(MenuItem, {
+    onClick: handleDownloadToPNG
+  }, "PNG"), /*#__PURE__*/React.createElement(MenuItem, {
+    onClick: handleDownloadToSVG
+  }, "SVG (not functional yet)"), /*#__PURE__*/React.createElement(MenuItem, {
+    onClick: handleDownloadToEPS
+  }, "EPS (not functional yet)"), /*#__PURE__*/React.createElement(MenuItem, {
+    onClick: handleDownloadToPDF
+  }, "PDF"))), /*#__PURE__*/React.createElement("div", {
+    className: props.style.graphDiv,
+    ref: printRef
+  }, /*#__PURE__*/React.createElement(Bar, {
+    id: "graph",
     data: graphData,
     options: options
-  }));
+  })));
 }
